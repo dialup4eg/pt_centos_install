78a29e4a3da34eef8623d6bd5d78db76d1757b8b
dialup4eg <34375614+dialup4eg@users.noreply.github.com>
2020-06-14 01:19:39 +0500

    update

--- install.sh
+++ install.sh
@@ -7,6 +7,12 @@ npm install pm2@latest -g
 cd /root/
 wget $(curl -s https://api.github.com/repos/taniman/profit-trailer/releases/latest | grep 'browser_download_url' | cut -d\" -f4)
 unzip ProfitTrailer*.zip && rm ProfitTrailer*.zip
-mv ProfitTrailer* ProfitTrailerBTC
+mv ProfitTrailer* PT-BTC
 firewall-cmd --zone=public --add-port=8081/tcp --permanent
 firewall-cmd --reload
+cd ~
+cd PT-BTC
+chmod +x ProfitTrailer.jar
+pm2 start pt2-ProfitTrailer.json
+pm2 save
+pm2 startup

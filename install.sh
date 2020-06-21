24b50b8549cdd3ebdb83398ad3d2eecd094a1043
dialup4eg <34375614+dialup4eg@users.noreply.github.com>
2020-06-21 06:23:02 +0500

    Несколько ботов

--- install.sh
+++ install.sh
@@ -8,7 +8,11 @@ npm install pm2@latest -g
 cd /root/
 wget $(curl -s https://api.github.com/repos/taniman/profit-trailer/releases/latest | grep 'browser_download_url' | cut -d\" -f4)
 unzip ProfitTrailer*.zip && rm ProfitTrailer*.zip
+# Следующей строчкой мы переименовываем каталог ProfitTrailer* в PT-BTC
 mv ProfitTrailer* PT-BTC
+#Если нужно несколько копий бота, то уберите # у строчек ниже и измените PT-ETH и PT-USDT на нужные.
+#cp PT-BTC PT-ETH
+#cp PT-BTC PT-USDT
 firewall-cmd --zone=public --add-port=8081/tcp --permanent
 firewall-cmd --reload
 cd ~
